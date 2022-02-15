657dfa7aa8b041e5c001534bca170f53b34d389e
dntech <danilojosue1999@gmail.com>
2022-02-16 00:00:30 +0100

    Add files via upload

---
+++ uart-ttl-writer.py
@@ -0,0 +1,29 @@
+
+from machine import UART
+import time
+uart = UART(2, 115200)  
+uart.init(115200, bits=8, parity=None, stop=1)
+
+while True:
+	d = input("write: ")
+	if d != '':
+		uart.write(f'{d}\r\n')
+	time.sleep(4)
+	str(uart.read().decode()).replace("\r\n", " ").replace('\x1b',' ').replace('[1;34m', '').replace('[1;36m', '').replace('[0m', '').replace('[0;0m','').replace('[1;32m', '').replace(d, '')
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
